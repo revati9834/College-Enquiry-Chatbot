f7a6b3cc55d86506177af36c124285d02526abd3
Revati Jadhav <71699081+revati9834@users.noreply.github.com>
2021-06-08 15:48:24 +0530

    update corpus data

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,6 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Creating ChatBot Instance
-
 chatbot = ChatBot(
     'CollegeBot',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
