4171ff5b8667b4002891fcf77a4309cca30992fc
Revati Jadhav <71699081+revati9834@users.noreply.github.com>
2021-06-08 15:55:14 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
 # Creating ChatBot Instance
 chatbot = ChatBot(
     'CollegeBot',
